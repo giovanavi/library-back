3a9033ff24a78f2b3ff52076cdb0683ae736a082
giovanavi <giovana.gvi02@gmail.com>
2023-01-28 20:00:04 -0300

    att @PostMapping

--- src/main/java/com/giovana/library/resources/EmprestimoResource.java
+++ src/main/java/com/giovana/library/resources/EmprestimoResource.java
@@ -2,7 +2,9 @@ package com.giovana.library.resources;
 
 import com.giovana.library.dto.EmprestimoDTO;
 import com.giovana.library.entity.Emprestimo;
+import com.giovana.library.entity.Livro;
 import com.giovana.library.services.EmprestimoService;
+import com.giovana.library.services.LivroService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,17 +20,20 @@ import java.util.stream.Collectors;
 public class EmprestimoResource {
 
     @Autowired
-    private EmprestimoService service;
+    private EmprestimoService emprestimoService;
+
+    @Autowired
+    private LivroService livroService;
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Emprestimo> findById(@PathVariable Integer id){
-        Emprestimo emp = service.findById(id);
+        Emprestimo emp = emprestimoService.findById(id);
         return ResponseEntity.ok().body(emp);
     }
 
     @GetMapping(value = "/")
     public ResponseEntity<List<EmprestimoDTO>> findAll(){
-        List<Emprestimo> list = service.findAll();
+        List<Emprestimo> list = emprestimoService.findAll();
 
         List<EmprestimoDTO> listDTO = list.stream().map(obj -> new EmprestimoDTO(obj)).toList();
 
@@ -36,7 +41,9 @@ public class EmprestimoResource {
     }
     @PostMapping
     public ResponseEntity<Emprestimo> create(@RequestBody Emprestimo emp){
-        emp = service.create(emp);
+        livroService.create(emp.getLivro());
+
+        emp = emprestimoService.create(emp);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(emp.getId()).toUri();
 
         return ResponseEntity.created(uri).body(emp);
--- src/main/java/com/giovana/library/services/EmprestimoService.java
+++ src/main/java/com/giovana/library/services/EmprestimoService.java
@@ -1,7 +1,9 @@
 package com.giovana.library.services;
 
 import com.giovana.library.entity.Emprestimo;
+import com.giovana.library.entity.Livro;
 import com.giovana.library.repositories.EmprestimoRepository;
+import com.giovana.library.repositories.LivroRepository;
 import com.giovana.library.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,19 +15,25 @@ import java.util.Optional;
 public class EmprestimoService {
 
     @Autowired
-    private EmprestimoRepository repository;
+    private EmprestimoRepository emprestimoRepository;
+    @Autowired
+    private LivroRepository livroRepository;
 
     public Emprestimo findById(Integer id){
-        Optional<Emprestimo> emp =  repository.findById(id);
+        Optional<Emprestimo> emp =  emprestimoRepository.findById(id);
         return emp.orElseThrow( () -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: "+id+" - Tipo: "+ Emprestimo.class.getName()));
     }
 
     public List<Emprestimo> findAll(){
-        return repository.findAll();
+        return emprestimoRepository.findAll();
     }
 
     public Emprestimo create(Emprestimo emp){
-        return repository.save(emp);
+        Livro livro = emp.getLivro();
+        livro.setStatus("true");
+        livroRepository.save(emp.getLivro());
+
+        return emprestimoRepository.save(emp);
     }
 }
