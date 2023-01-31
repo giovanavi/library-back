3b87ed1542bf567a9755f2caab63cf747f2d70d5
giovanavi <giovana.gvi02@gmail.com>
2023-01-30 21:21:49 -0300

    att

--- src/main/java/com/giovana/library/entity/Emprestimo.java
+++ src/main/java/com/giovana/library/entity/Emprestimo.java
@@ -1,9 +1,7 @@
 package com.giovana.library.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
